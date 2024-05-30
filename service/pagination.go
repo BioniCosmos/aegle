4fc7b7bf5141a32b78543de78dc1109ddb6869f3
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2024-05-30 15:50:55 +0800

    Remove unnecessary fields in `Pagination`
    The `Pagination` component of shadcn-vue can do all for us.

--- service/pagination.go
+++ service/pagination.go
@@ -8,16 +8,8 @@ import (
 )
 
 type Pagination[T any] struct {
-	Items        []T  `json:"items,omitempty"`
-	CurrentPage  int  `json:"currentPage,omitempty"`
-	PageSize     int  `json:"pageSize,omitempty"`
-	TotalPages   int  `json:"totalPages,omitempty"`
-	HasPrevious  bool `json:"hasPrevious,omitempty"`
-	HasNext      bool `json:"hasNext,omitempty"`
-	PreviousPage int  `json:"previousPage,omitempty"`
-	NextPage     int  `json:"nextPage,omitempty"`
-	FirstPage    int  `json:"firstPage,omitempty"`
-	LastPage     int  `json:"lastPage,omitempty"`
+	Items []T `json:"items"`
+	Total int `json:"total"`
 }
 
 const pageSize = 10
@@ -34,24 +26,17 @@ func paginate[T any](
 	if err != nil {
 		return Pagination[T]{}, err
 	}
-	totalPages := int(total) / pageSize
-	if int(total)%pageSize != 0 {
-		totalPages++
-	}
 	items, err := f(
 		options.Find().SetSkip(int64((page - 1) * pageSize)).SetLimit(pageSize),
 	)
 	if err != nil {
 		return Pagination[T]{}, err
 	}
+	if items == nil {
+		items = make([]T, 0)
+	}
 	return Pagination[T]{
-		Items:        items,
-		CurrentPage:  page,
-		PageSize:     pageSize,
-		TotalPages:   totalPages,
-		HasPrevious:  page > 1,
-		HasNext:      page < totalPages,
-		PreviousPage: max(page-1, 1),
-		NextPage:     min(page+1, totalPages),
+		Items: items,
+		Total: int(total),
 	}, nil
 }
